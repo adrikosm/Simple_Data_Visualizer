178bda3fef7026e860c93bfd4010d3a4ab536f67
Andreas Mavropoulos <adrikosm@gmail.com>
2021-05-05 13:06:59 +0200

    Updated main

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ class Aplication(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("CSV Viewer")
-        
+
         # Data frame for csv
         self.data_frame = tk.LabelFrame(self,text="Data")
         self.data_frame.place(height=600,width=900,relwidth=0.53,relheight=0.6)
@@ -32,26 +32,26 @@ class Aplication(tk.Tk):
 
         return filename
 
-
-def load_data(file_name):
-    """
-    Loads csv data into a pandas dataframe
-    and returns the dataframe
-    """
-    file_path = file_name
-    try:
-        csv_filename = r"{}".format(file_path)
-        if csv_filename[-4:] == ".csv":
-            df = pd.read_csv(csv_filename)
-            return df
-        else:
-            df = None
-    except ValueError:
-        tk.messagebox.showerror("Information","The file you have given is invalid")
-        return None
-    except FileNotFoundError:
-        tk.messagebox.showerror("Information",f"No file found {file_path}")
-        return None
+    
+    def load_data(self,master,filename):
+        """
+        Loads csv data into a pandas dataframe
+        and returns the dataframe
+        """
+        file_path = filename
+        try:
+            csv_filename = r"{}".format(file_path)
+            if csv_filename[-4:] == ".csv":
+                data_frame = pd.read_csv(csv_filename)
+                return data_frame
+            else:
+                data_frame = None
+        except ValueError:
+            master.messagebox.showerror("Information","The file you have given is invalid")
+            return None
+        except FileNotFoundError:
+            master.messagebox.showerror("Information",f"No file found {file_path}")
+            return None
 
 
 
